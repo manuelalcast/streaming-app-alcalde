bce2b01b6f736ea872407533f336db27900c3b0c
Manuel Alcalde <manuel.alcast@gmail.com>
2023-05-14 00:52:38 -0500

    last step

---
+++ src/components/Confetti.js
@@ -0,0 +1,82 @@
+import { useCallback, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/streaming.css";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+export default function Confetti() {
+  let navigate = useNavigate();
+  const refAnimationInstance = useRef(null);
+
+  const getInstance = useCallback((instance) => {
+    refAnimationInstance.current = instance;
+  }, []);
+
+  const makeShot = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      });
+  }, []);
+
+  useEffect(() => {
+    fire();
+  });
+
+  const fire = useCallback(() => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot(0.2, {
+      spread: 60,
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }, [makeShot]);
+
+  return (
+    <div>
+      <ReactCanvasConfetti refConfetti={getInstance} className="confetti" />
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="relative p-6 flex-auto">
+              <h1 className="text-2xl font-title">Película alquilada</h1>
+              <p className="my-4 text-slate-500 text-lg leading-relaxed">
+              </p>
+            </div>
+            <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
+              <button
+                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => navigate("/")}
+              >
+                Culminar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </div>
+  );
+}
--- src/views/MovieRent.js
+++ src/views/MovieRent.js
@@ -4,9 +4,9 @@ import rentIcon from "../assets/rent-icon.svg";
 import Back from "../assets/back.svg";
 import Plus from "../assets/plus.svg";
 import Less from "../assets/less.svg";
-import Claqueta from "../assets/clapperboard.svg";
 import React, { useState } from "react";
 import useCounterDays from "../hooks/useCounterDays";
+import Confetti from '../components/Confetti';
 
 export const MovieRent = () => {
   let navigate = useNavigate();
@@ -89,39 +89,9 @@ export const MovieRent = () => {
           </div>
         </div>
       </div>
-      {statusRentMovie ? (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <h1 className="text-2xl font-title">Película alquilada</h1>
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    <img
-                      class="w-50-clap h-50-clap m-auto"
-                      src={Claqueta}
-                      alt="img-rent"
-                    />
-                  </p>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => navigate("/")}
-                  >
-                    Culminar
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+      {statusRentMovie &&  <Confetti />}
+        {/*
+      ) : null}*/}
     </div>
   );
 };
