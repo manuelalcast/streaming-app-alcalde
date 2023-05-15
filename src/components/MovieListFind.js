7c6e1b88d31d9cdd0003993e3e9695bc0aa2bea6
Manuel Alcalde <manuel.alcast@gmail.com>
2023-05-14 23:04:00 -0500

    find movie - image

--- src/components/MovieListFind.js
+++ src/components/MovieListFind.js
@@ -3,12 +3,13 @@ import { useNavigate } from "react-router-dom";
 function Movie({ movie }) {
   let navigate = useNavigate();
   return (
-    <div className="container-image-find">
+    <div  className="container-image-find"
+          onClick={() => navigate("/movie", { state: movie })}
+    >
       <img
         src={movie.images[0]}
         alt="pic"
         className="h-25 w-72 rounded-md shadow-2xl transform -translate-y-4 border-4 border-gray-300 shadow-lg m-auto cursor-pointer"
-        onClick={() => navigate("/movie", { state: movie })}
       />
       <span className="bottom-right-find font-title cursor-pointer">{movie.name}</span>
     </div>
